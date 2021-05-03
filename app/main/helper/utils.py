62963305317eeee842bfdf46125eab6ca7a39792
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-05-03 07:17:15 -0300

    Include Access-Control-Request-Method to response

--- app/main/helper/utils.py
+++ app/main/helper/utils.py
@@ -38,4 +38,4 @@ def create_response(status, msg, code):
         'status': status,
         'message': msg,
     }
-    return response_object, code, {'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'}
+    return response_object, code, {'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept', 'Access-Control-Request-Method': 'POST'}
