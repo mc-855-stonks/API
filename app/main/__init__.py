24d836ea3a7992e481ea002851b9f134fc102e26
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-05-03 08:48:27 -0300

    Using CORS lib to enable cross origin preflight requests

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from app.main.config import config_by_env
+from flask_cors import CORS
 
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
@@ -11,5 +12,6 @@ def create_app(env):
     app.config.from_object(config_by_env[env])
     db.init_app(app)
     flask_bcrypt.init_app(app)
+    CORS(app, allow_headers='Content-Type')
 
     return app
