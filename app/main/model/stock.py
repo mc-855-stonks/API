07c26478c70cd5eb08960ee59d1e4f07b1bb3465
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2021-05-19 23:17:58 -0300

    update migrations

--- app/main/model/stock.py
+++ app/main/model/stock.py
@@ -8,10 +8,10 @@ class Stock(db.Model):
     __tablename__ = 'stock'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.Unicode, nullable=False, default="''")
-    ticker = db.Column(db.Unicode, nullable=False, default="''")
-    segment = db.Column(db.Unicode, nullable=False, default="''")
-    image = db.Column(db.Unicode, nullable=False, default="''")
+    name = db.Column(db.Unicode, nullable=True)
+    ticker = db.Column(db.Unicode, nullable=True)
+    segment = db.Column(db.Unicode, nullable=True)
+    image = db.Column(db.Unicode, nullable=True)
 
     def __repr__(self):
         return '<stock({}): name: {}, ticker: {}'.format(self.id, self.name, self.ticker)
