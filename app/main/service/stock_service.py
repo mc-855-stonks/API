ac63e314f0e4314e0209e6c442f509598dc19270
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2021-05-21 19:44:40 -0300

    fix stocks api

--- app/main/service/stock_service.py
+++ app/main/service/stock_service.py
@@ -1,6 +1,12 @@
 from app.main import db
 from app.main.model.stock import Stock
 
+
 def get_available_stocks():
     stocks = db.session.query(Stock).all()
-    return stocks
+    return [{
+        'name': s.name,
+        'ticker': s.ticker,
+        'segment': s.segment,
+        'image': s.image
+    } for s in stocks]
