811393a833e79b2dc73eae69bf4326684ff37abf
Gabriel <gabrielsantosrv@outlook.com>
2021-05-22 00:20:31 -0300

    correcting return of get_available_stocks´´ method

--- app/main/service/stock_service.py
+++ app/main/service/stock_service.py
@@ -3,11 +3,4 @@ from app.main.model.stock import Stock
 
 
 def get_available_stocks():
-    stocks = db.session.query(Stock).all()
-    return [{
-        'name': s.name,
-        'ticker': s.ticker,
-        'segment': s.segment,
-        'image': s.image,
-        'id': s.id
-    } for s in stocks]
+    return db.session.query(Stock).all()
