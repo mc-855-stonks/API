f2826b413bb0facbbf697c9ffcf6d481164a7f4f
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2021-05-23 16:22:37 -0300

    remove id

--- app/main/dto/stock_dto.py
+++ app/main/dto/stock_dto.py
@@ -4,7 +4,6 @@ from flask_restx import Namespace, fields
 class StockDto:
     api = Namespace('stock', description='retrieve available stocks')
     stock = api.model('stock', {
-        'id': fields.Integer(required=False, description='stock id'),
         'name': fields.String(required=False, description='stock name'),
         'ticker': fields.String(required=False, description='ticker'),
         'segment': fields.String(required=False, description='company segment'),
